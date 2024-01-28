5c6e7c36f446d2ff64a2db7bc1429240e0a4ecb2
Fry <renyuan.fei.1020@gmail.com>
2024-01-28 20:57:19 +1030

    Add FontAwesome for glyphicons support
    A new file has been added src/IdentityService/wwwroot/lib/bootstrap4-glyphicons/maps/glyphicons-fontawesome.less to support FontAwesome for glyphicons. This implements the use of Font Awesome icons as glyphs in Bootstrap 4 by mapping the previous Bootstrap glyph classes to their equivalent Font Awesome classes. The mapping spans over 3800 lines covering a wide array of icons.

---
+++ src/IdentityService/HostingExtensions.cs
@@ -0,0 +1,91 @@
+using Duende.IdentityServer;
+using Duende.IdentityServer.Services;
+using IdentityService.Data;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace IdentityService;
+
+internal static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        builder.Services
+               .AddIdentityServer(options =>
+               {
+                   options.Events.RaiseErrorEvents = true;
+                   options.Events.RaiseInformationEvents = true;
+                   options.Events.RaiseFailureEvents = true;
+                   options.Events.RaiseSuccessEvents = true;
+
+                   if (builder.Environment.IsEnvironment("Docker"))
+                   {
+                       options.IssuerUri = "identity-svc";
+                   }
+
+                   if (builder.Environment.IsProduction())
+                   {
+                       options.IssuerUri = "https://id.trycatchlearn.com";
+                   }
+
+                   // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+                   // options.EmitStaticAudienceClaim = true;
+               })
+               .AddInMemoryIdentityResources(Config.IdentityResources)
+               .AddInMemoryApiScopes(Config.ApiScopes)
+               .AddInMemoryClients(Config.Clients(builder.Configuration))
+               .AddAspNetIdentity<ApplicationUser>();
+
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.SameSite = SameSiteMode.Lax;
+        });
+
+        builder.Services.AddAuthentication();
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        if (app.Environment.IsProduction())
+        {
+            app.Use(async (ctx, next) =>
+            {
+                var serverUrls = ctx.RequestServices.GetRequiredService<IServerUrls>();
+                serverUrls.Origin = serverUrls.Origin = "https://id.trycatchlearn.com";
+                await next();
+            });
+        }
+
+
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
+        app.MapRazorPages()
+            .RequireAuthorization();
+
+        return app;
+    }
+}
