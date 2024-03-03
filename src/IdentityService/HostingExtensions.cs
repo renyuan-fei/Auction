3b02e1ff8a0faa32213508dc8b9914bf196821cd
Fry <renyuan.fei.1020@gmail.com>
2024-03-04 03:02:12 +1030

    Add Kubernetes configuration files and workflows
    Created necessary Kubernetes deployment configurations by adding PersistentVolumeClaim files for various services. Also added deployments and services for the applications in the K8S folder. Updated HostingExtensions.cs and Added CI/CD workflows in the .github/workflows directory for automatic deployment.

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -41,7 +41,7 @@ internal static class HostingExtensions
 
              if (builder.Environment.IsProduction())
              {
-               options.IssuerUri = "https://id.trycatchlearn.com";
+               options.IssuerUri = "https://id.auction.frankfei.me";
              }
 
              // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
@@ -78,7 +78,7 @@ internal static class HostingExtensions
       app.Use(async (ctx, next) =>
       {
         var serverUrls = ctx.RequestServices.GetRequiredService<IServerUrls>();
-        serverUrls.Origin = serverUrls.Origin = "https://id.trycatchlearn.com";
+        serverUrls.Origin = serverUrls.Origin = "https://id.auction.frankfei.me";
         await next();
       });
     }
